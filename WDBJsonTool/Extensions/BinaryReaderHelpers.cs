27b513e0fa88bb814bf3a5290027b09358e711e8
Surihix <neweramysterymailer@gmail.com>
2024-09-12 01:58:58 +0530

    added utf8 encoding for reading strings from bytes

--- WDBJsonTool/Extensions/BinaryReaderHelpers.cs
+++ WDBJsonTool/Extensions/BinaryReaderHelpers.cs
@@ -79,7 +79,7 @@ public static class BinaryReaderHelpers
         var readValueBuffer = reader.ReadBytes(readCount);
         ReverseIfBigEndian(isBigEndian, readValueBuffer);
 
-        return Encoding.ASCII.GetString(readValueBuffer).Replace("\0", "");
+        return Encoding.UTF8.GetString(readValueBuffer).Replace("\0", "");
     }
 
 
